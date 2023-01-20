977dea605d4b7bc33dba72f13dc5599286c3dea5
Whatk <i@whatk.me>
2023-01-20 18:39:35 +0800

    feat: version file

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,6 @@ import { resolve } from "path";
 const TEMP_ENV = Deno.env.get("temp");
 const AUTOIT3WRAPPER_PATH_ENV = Deno.env.get("AUTOIT3WRAPPER_PATH");
 
-export const TIS_VERSION = "0.1.1";
 export const TIS_BUILDTIME = new Date().toISOString().toString();
 export const TWEAK_RULE_PATH = "tweaks";
 export const TEMPDIR = TEMP_ENV
